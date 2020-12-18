c7a7b13d8a136b6a8b0ffa8e102ab8d6efcd19d3
bob wang <xingbo.wang@winndoo.com>
2020-12-18 12:00:19 +0800

    predict

--- predict.py
+++ predict.py
@@ -1,11 +1,14 @@
 import os
-from albert_sentiment import model, test_generator
+from albert_sentiment import model, test_data, data_generator
 
-def gen_output():
+def gen_output(test_generator):
     for x, y in test_generator:
         y_pred = model.predict(x)
         print(y_pred)
         print(y)
 
 if __name__ == "__main__":
-    gen_output()
+
+    batch = 10000
+    test_generator = data_generator(test_data, batch)
+    gen_output(test_generator)

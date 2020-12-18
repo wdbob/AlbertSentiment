a3acf35107a65764615e833c25a74e8dc04da972
bob wang <xingbo.wang@winndoo.com>
2020-12-18 13:38:13 +0800

    update

--- predict.py
+++ predict.py
@@ -1,15 +1,20 @@
 import os
 from albert_sentiment import model, test_data, data_generator
+import numpy as np
 
 def gen_output(test_generator):
     l = len(test_generator)
     print('length of test data: ', l)
     cnt = 0
+    pred = np.array([]).reshape(0,2)
+    label = np.array([]).reshape(-1)
     for x, y in test_generator:
         y_pred = model.predict(x)
-        cnt += len(x)
-        print(cnt, '/', l)
-        print(type(y), type(y_pred))
+        cnt += 1
+        print(str(cnt)+ '/'+ str(l))
+        pred = np.concatenate((pred, y_pred))
+        label = np.concatenate((label, y))
+        print(pred.shape, label.shape)
 
 
 if __name__ == "__main__":

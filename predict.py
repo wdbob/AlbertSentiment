000b9bbab715a32757d9e162ef609b2adb23d893
bob wang <xingbo.wang@winndoo.com>
2020-12-18 12:09:49 +0800

    predict

--- predict.py
+++ predict.py
@@ -2,13 +2,18 @@ import os
 from albert_sentiment import model, test_data, data_generator
 
 def gen_output(test_generator):
+    l = len(test_generator)
+    print('length of test data: ', l)
+    cnt = 0
     for x, y in test_generator:
         y_pred = model.predict(x)
-        print(y_pred)
-        print(y)
+        cnt += len(x)
+        print(cnt, '/', l)
+        print(type(y), type(y_pred))
+
 
 if __name__ == "__main__":
 
-    batch = 10000
+    batch = 1000
     test_generator = data_generator(test_data, batch)
     gen_output(test_generator)
